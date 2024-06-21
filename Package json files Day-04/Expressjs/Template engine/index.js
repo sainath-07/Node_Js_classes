d91777434f6c008876ffe9f00d5a4eb897077c6a
sainath-07 <sainathb308@gmail.com>
2024-06-21 11:01:03 +0530

    Template engine

---
+++ Package json files Day-04/Expressjs/Template engine/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const filepath=path.join(__dirname,'/public')
+
+// console.log(filepath)
+
+app.get('/contact',(req,resp)=>{
+    resp.sendFile(`${filepath}/contact.html`)
+    
+})
+
+
+
+// By using the set method we will set the template engine 
+
+app.set('view engine','ejs')
+
+
+// to set the template engine we need to pass the first parameter as view engine and second parameter as package name like ejs
+
+
+
+
+// steps to display EJS file
+
+// 1. we need to pass the first parameter as view engine
+// 2. name of the package engine
+// 3. we will access the ejs files using the router method in the place of the sendfile
+
+app.get('/profile',(req,resp)=>{
+    const user={
+        name :"sainath",
+        email : "sainath@gmail.com",
+        city : "hyd"
+    }
+     resp.render('profile',{user})
+})
+
+app.listen(5000)
